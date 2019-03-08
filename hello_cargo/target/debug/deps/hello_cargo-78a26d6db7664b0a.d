696deca9557d313f750dbf3ebdda0e9334922d0e
kranz fontanilla <me@kranznikolai.com>
2019-03-08 13:42:55 -0500

    added a new project

---
+++ hello_cargo/target/debug/deps/hello_cargo-78a26d6db7664b0a.d
@@ -0,0 +1,5 @@
+C:\Users\Kranz.nitronint\projects\hello_cargo\target\debug\deps\hello_cargo-78a26d6db7664b0a.exe: src\main.rs
+
+C:\Users\Kranz.nitronint\projects\hello_cargo\target\debug\deps\hello_cargo-78a26d6db7664b0a.d: src\main.rs
+
+src\main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+C:\Users\Kranz.nitronint\projects\hello_cargo\target\debug\hello_cargo.pdb: C:\Users\Kranz.nitronint\projects\hello_cargo\src\main.rs
