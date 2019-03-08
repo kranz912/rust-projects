237e591be515be26c3474a28e5736fa69997fec0
kranz fontanilla <me@kranznikolai.com>
2019-03-08 14:05:32 -0500

    added random event

--- guessthenumber/src/main.rs
+++ guessthenumber/src/main.rs
@@ -2,11 +2,15 @@
 //std               -- standard library
 //io                -- input/output
 use  std::io;
+use  rand::Rng;
+use  std::cmp::Ordering;
 //fn                -- function declaration
 //main              -- function name * main is the entry point of your program
 fn main() {
     //printlln! -- macro for printing to screen
     println!("Guess the number");
+    let _secret_number = rand::thread_rng().gen_range(1,101);
+
     println!("Please input your guess");
     //let           -- variable declaration
     //mut           -- mutability indicates that a variable is mutable
@@ -17,5 +21,16 @@ fn main() {
     //&mut guess    -- pass input value as a reference to guess variable
     //expect()      -- error handler
     io::stdin().read_line(&mut guess).expect("failed to read line");
+
+    let guess: u32 = guess.trim().parse()
+        .expect("Please Type a Number");
+
     println!("You guess: {}", guess);
+
+    match guess.cmp(&_secret_number){
+        Ordering::Less =>println!("Too Small"),
+        Ordering::Greater => println!("Too Big"),
+        Ordering::Equal =>println!("You Win")
+    }
+    
 }
