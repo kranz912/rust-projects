5751047a16a1c8cc6b8cf19f02ca1b360c2030ee
kranz fontanilla <me@kranznikolai.com>
2019-03-08 22:08:02 -0500

    added shadowing

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -14,7 +14,7 @@ fn main() {
 
     /*
        Mutable Variables
-       -- Unlike immutable variables, mutable variables allows us to change it's value
+       --   Unlike immutable variables, mutable variables allows us to change it's value
        example of a mutable variable:
     */
     let mut _z =10;
@@ -23,11 +23,30 @@ fn main() {
 
     /*
         Constant Variables
-        -- Constants are immutable variables
-        -- you can't declare a mutable constant
-        -- Constants can be declared in the global scope
-        -- Data type is required when declaring a constant variable
+        --  Constants are immutable variables
+        --  you can't declare a mutable constant
+        --  Constants can be declared in the global scope
+        --  Data type is required when declaring a constant variable
     */
     const MAX_POINTS: u32 = 300_000;
     println!("MAX_POINTS: {}",MAX_POINTS);
+    /*
+        Shadowing
+        --  Creating a copy of the same variable with the same name
+        --  the difference between mutable and shadowing
+        ----    shadowing creates a new instance of that variable into memory (changes the reference of that variable)
+        ----    mutable changes the value of a variable while retaining it's reference
+        example of shadowing:
+    */
+    // assign 5 to s
+    let s = 5;
+    println!("s: {}",s);
+
+    // s is now 15
+    let s = s + 10;
+    println!("s: {}",s);
+
+    // s is now 30
+    let s = s * 2;
+    println!("s: {}",s);
 }
